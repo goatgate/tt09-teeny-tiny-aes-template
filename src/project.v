27a23b79dc240aa516cd7fcc57b5b63f0df9adf2
goatgate <sriram.nimmala@icloud.com>
2024-11-07 18:58:07 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt09_ccu_sriram (
+module tt09_ccu_goatgate (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
