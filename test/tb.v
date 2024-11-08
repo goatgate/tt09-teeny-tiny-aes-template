168d98e4e0c0cde6b5f47d3dc8d46bc8446158bf
goatgate <sriram.nimmala@icloud.com>
2024-11-07 19:25:12 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt09_ccu_goatgate user_project (
+  tt_um_ccu_goatgate user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
